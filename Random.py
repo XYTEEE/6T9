2aa197b24fead3f67caf09c4588dbf79624c09e0
EVERYTHING' FUCK <104085499+XYTEEE@users.noreply.github.com>
2022-12-25 15:36:00 +0600

    Create Random.py

---
+++ Random.py
@@ -0,0 +1,16 @@
+import os, platform
+from os import path,system
+from platform import uname
+arch=uname().machine.lower()
+os.system('git pull')
+if 'aarch' in arch:
+    print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
+    if path.isfile("XD.so"):
+        pass
+    else:
+        system("curl -L https://raw.githubusercontent.com/XYTEEE/files/main/sex.so -o sex.so")
+    else:
+        os.system('xdg-open https://www.facebook.com/ps7c8o.p133h1')
+else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
+import sex
+sex.main()
