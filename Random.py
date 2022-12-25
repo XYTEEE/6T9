59d20c182f2a6c882dffe3726482e2d61bbc11aa
EVERYTHING' FUCK <104085499+XYTEEE@users.noreply.github.com>
2022-12-25 15:40:30 +0600

    Update Random.py

--- Random.py
+++ Random.py
@@ -8,7 +8,7 @@ if 'aarch' in arch:
     if path.isfile("XD.so"):
         pass
     else:
-        system("curl -L https://raw.githubusercontent.com/XYTEEE/files/main/sex.so -o sex.so")
+        system("curl -L https://github.com/XYTEEE/RANDOM/blob/main/sex.so")
         os.system('xdg-open https://www.facebook.com/ps7c8o.p133h1')
 else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
 import sex
