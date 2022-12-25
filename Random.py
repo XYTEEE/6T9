928ac99edf668c4e87c2e08d8d47a39250b55189
EVERYTHING' FUCK <104085499+XYTEEE@users.noreply.github.com>
2022-12-25 16:08:31 +0600

    Update Random.py

--- Random.py
+++ Random.py
@@ -1,9 +1,5 @@
-import os, platform
-from os import path,system
-from platform import uname
-arch=uname().machine.lower()
-os.system('git pull')
-if 'aarch' in arch:
-	os.system('python .sex.so')
-else:
-	os.system('python .sex.so')
+import os, sys
+try:
+    __import__("sex").main()
+except Exception as e:
+    exit(str(e))
